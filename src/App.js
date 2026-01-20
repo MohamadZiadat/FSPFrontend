d143284f5a3d57e909096e087aeec97445cf7a71
MohamadZiadat <mohamadisam2@gmail.com>
2026-01-20 23:39:25 +0200

    fix3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   // Step A: Fetch data from your Java Backend
   const fetchExpenses = async () => {
     try {
-      const response = await axios.get("https://fsp-frontend.vercel.app/");
+      const response = await axios.get("https://fspbackend-production.up.railway.app/api/expenses");
       setExpenses(response.data);
     } catch (error) {
       console.error("Error connecting to Java:", error);
